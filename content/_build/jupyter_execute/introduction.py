6aa483045829edc35a35c61255813889bf52ce70
Cale Kochenour <cale.kochenour@alumni.psu.edu>
2020-11-08 20:11:34 -0700

    Add bibliography

--- content/_build/jupyter_execute/introduction.py
+++ content/_build/jupyter_execute/introduction.py
@@ -1,6 +1,6 @@
 # Introduction
 
-This book provides text and examples for how to use [Google Earth Engine](https://earthengine.google.com/) to work through the remote sensing data workflow:
+This book provides text and examples for how to use Google Earth Engine {cite}`gorelick2017google` to work through the remote sensing data workflow:
 
 * Acquisition and Preprocessing;
 * Processing;
@@ -10,12 +10,24 @@ This book provides text and examples for how to use [Google Earth Engine](https:
 
 The book includes code in `JavaScript` and `Python` languages for completing remote sensing analyses.
 
+## Content
+
 Part 1 (beginner) provides introductory modules and activities to introduce Earth Engine functionality. Each example show a few tasks and processes to familiarize the reader with the Earth Engine platform and syntax.
 
 Part 2 (intermediate) provides workflows that piece together tasks and processes from Part 2 in order to create more complex and meaningful workflows, and introduces more complex tasks and processes.
 
 Part 3 (advanced) provides full scientific workflows in Earth Engine that have been re-created from existing, peer-reviewed papers and/or ongoing remote sensing projects. One of the goals of this book it to promote and provide open and reproducible science workflows, and this chapter is dedicated to creating this type of workflow within the Earth Engine platform.
 
+## Prerequisites
+
+In order to reproduce the computational workflows in this book, you must have an active [Google Earth Engine account](https://earthengine.google.com/).
+
+
+## Bibliography
+
+```{bibliography} references.bib
+:style: alpha
+```
 
 
 ```{toctree}
